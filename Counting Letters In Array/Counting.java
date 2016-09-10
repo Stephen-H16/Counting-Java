da0d3101e6d997c799bc8e22618452c36c47c433
Stephen Hoey <stephenjhoey@gmail.com>
2016-09-10 10:46:16 +0100

    Add files via upload

---
+++ Counting Letters In Array/Counting.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Counting {
+
+    static String[] letters = new String[3];
+    private static int count;
+
+    public static void countElements() {
+        for (int i = 0; i < letters.length; i++) {
+            int count = 0;
+            for (int j = 0; j < letters.length; j++) {
+                if (letters[j].equalsIgnoreCase(letters[i])) {
+                    count++;
+                }
+            }
+            System.out.println("Word = " + letters[i].toUpperCase() + " is repeted " + count + " times");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Add Three Letters To the Array Then Type -1");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.next();
+
+        while (!("-1").equals(input)) {
+            letters[count] = input;
+            count++;
+            input = scanner.next();
+        }
+        countElements();
+    }
+}
